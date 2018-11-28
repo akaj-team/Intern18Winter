d1111477c77785f47cade7db831778232be0288b
at-dinhtruong <dinh.truong@asiantech.vn>
2018-11-28 16:13:49 +0700

    commitActivityFragment#1

--- app/src/at-dinhtruong/java/asiantech/internship/summer/dinhtruong/debug/LoginActivity.java
+++ app/src/at-dinhtruong/java/asiantech/internship/summer/dinhtruong/debug/LoginActivity.java
@@ -2,11 +2,12 @@ package asiantech.internship.summer.dinhtruong.debug;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import asiantech.internship.summer.R;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
