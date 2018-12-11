d89171a787c21b67ed3fe3df98abf72ad93a8962
Hung Tran V.[2] <hung.tran2@asiantech.vn>
2018-12-11 17:46:11 +0700

    W1 D3 View and View Group

--- app/src/at-hungtran/java/asiantech/internship/summer/view/HomeActivity.java
+++ app/src/at-hungtran/java/asiantech/internship/summer/view/HomeActivity.java
@@ -27,12 +27,12 @@ public class HomeActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(this, COLUMNS));
-        data.add(new Data("A city Living Under the Shadow", R.mipmap.ic_avt, "RBC News", "#e1dfdf"));
-        data.add(new Data("The Problem for Democratic Leaders", R.mipmap.ic_avt1, "NY Times", "#f9f9f9"));
-        data.add(new Data("The Golden Secret to Better Beakfast", R.mipmap.ic_avt2, "BBC World", "#f9f9f9"));
-        data.add(new Data("How to Plan Your First Ski Vacation", R.mipmap.ic_avt3, "NBC Nightly", "#e1dfdf"));
-        data.add(new Data("How Social Isolation Is Killing Us", R.mipmap.ic_avt4, "RBC News", "#e1dfdf"));
-        data.add(new Data("The Labels to Sort Messages in Facebook", R.mipmap.ic_avt5, "BBC World", "#f9f9f9"));
+        data.add(new Data("A city Living Under the Shadow", R.drawable.ic_avt, "RBC News", "#e1dfdf"));
+        data.add(new Data("The Problem for Democratic Leaders", R.drawable.ic_avt1, "NY Times", "#f9f9f9"));
+        data.add(new Data("The Golden Secret to Better Beakfast", R.drawable.ic_avt2, "BBC World", "#f9f9f9"));
+        data.add(new Data("How to Plan Your First Ski Vacation", R.drawable.ic_avt3, "NBC Nightly", "#e1dfdf"));
+        data.add(new Data("How Social Isolation Is Killing Us", R.drawable.ic_avt4, "RBC News", "#e1dfdf"));
+        data.add(new Data("The Labels to Sort Messages in Facebook", R.drawable.ic_avt5, "BBC World", "#f9f9f9"));
         mNewsAdapter = new NewsAdapter(data);
         recyclerView.setAdapter(mNewsAdapter);
     }
