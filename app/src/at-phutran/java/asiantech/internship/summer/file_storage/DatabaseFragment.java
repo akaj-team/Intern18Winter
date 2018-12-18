9b958bd20d49f635d6e01d759a85cf727b46e8a8
Phu Tran D.[4] <phu.tran4@asiantech.vn>
2018-12-18 10:39:47 +0700

    file storage

--- app/src/at-phutran/java/asiantech/internship/summer/file_storage/DatabaseFragment.java
+++ app/src/at-phutran/java/asiantech/internship/summer/file_storage/DatabaseFragment.java
@@ -1,6 +1,5 @@
 package asiantech.internship.summer.file_storage;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
