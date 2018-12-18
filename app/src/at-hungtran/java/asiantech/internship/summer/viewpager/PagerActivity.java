5c9e48587b6ca6217f73c969abd3832d0f752efd
Hung Tran V.[2] <hung.tran2@asiantech.vn>
2018-12-18 16:02:32 +0700

    w2 d2 viewpager

--- app/src/at-hungtran/java/asiantech/internship/summer/viewpager/PagerActivity.java
+++ app/src/at-hungtran/java/asiantech/internship/summer/viewpager/PagerActivity.java
@@ -1,6 +1,5 @@
 package asiantech.internship.summer.viewpager;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -8,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import asiantech.internship.summer.R;
 
-@SuppressLint("Registered")
 public class PagerActivity extends AppCompatActivity {
     private OnChangingFavoritesListener onChangingFavoritesListener;
 
