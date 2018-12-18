ccc1c46bf9c8025184ee6a5d8f527c4f5b4fe290
at-dinhtruong <dinh.truong@asiantech.vn>
2018-12-18 08:23:04 +0700

    add progressbar dialog in activity

--- app/src/at-dinhtruong/java/asiantech/internship/summer/asynctaskthreadhandler/AsyncTaskThreadHandlerActivity.java
+++ app/src/at-dinhtruong/java/asiantech/internship/summer/asynctaskthreadhandler/AsyncTaskThreadHandlerActivity.java
@@ -1,19 +1,22 @@
 package asiantech.internship.summer.asynctaskthreadhandler;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import com.squareup.picasso.Picasso;
+import java.io.InputStream;
 
 import asiantech.internship.summer.R;
 
 public class AsyncTaskThreadHandlerActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String URL_IMAGE = "https://9mobi.vn/cf/images/2015/03/nkk/anh-dep-1.jpg";
     private ImageView mImgImageDownload;
-    String URL = "https://9mobi.vn/cf/images/2015/03/nkk/anh-dep-1.jpg";
     Context context;
 
     @Override
@@ -27,6 +30,42 @@ public class AsyncTaskThreadHandlerActivity extends AppCompatActivity implements
 
     @Override
     public void onClick(View view) {
-        Picasso.get().load(URL).into(mImgImageDownload);
+        new DownloadImageTask(mImgImageDownload).execute(URL_IMAGE);
     }
+
+    public DownloadTask(Context context) {
+        this.context = context;
+        mProgressDialog = new ProgressDialog(context);
+
+        mProgressDialog.setMessage("Downloading file..");
+        mProgressDialog.setIndeterminate(false);
+        mProgressDialog.setMax(100);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        mProgressDialog.setCanceledOnTouchOutside(false);
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap bmp = null;
+            try {
+                InputStream inputStream = new URL(urldisplay).openStream();
+                bmp = BitmapFactory.decodeStream(inputStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bmp;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+
 }
