fc88c51b5d7e7feaa8beab35fcb62de25a8aa691
Hung Tran V.[2] <hung.tran2@asiantech.vn>
2018-12-19 13:37:14 +0700

    update HomeActivity

--- app/src/at-hungtran/java/asiantech/internship/summer/view/HomeActivity.java
+++ app/src/at-hungtran/java/asiantech/internship/summer/view/HomeActivity.java
@@ -1,37 +1,45 @@
 package asiantech.internship.summer.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.view.View;
+import android.widget.Button;
 
 import asiantech.internship.summer.R;
-import asiantech.internship.summer.model.Data;
+import asiantech.internship.summer.eventlistener.LoginActivity;
 
-public class HomeActivity extends AppCompatActivity {
-    private static final int COLUMNS = 2;
-    List<Data> data = new ArrayList<>();
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_layoutfb);
+        setContentView(R.layout.activity_home);
         initView();
     }
 
-    public void initView() {
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, COLUMNS));
-        data.add(new Data("A city Living Under the Shadow", R.drawable.ic_avt, "RBC News", "#e1dfdf"));
-        data.add(new Data("The Problem for Democratic Leaders", R.drawable.ic_avt1, "NY Times", "#f9f9f9"));
-        data.add(new Data("The Golden Secret to Better Beakfast", R.drawable.ic_avt2, "BBC World", "#f9f9f9"));
-        data.add(new Data("How to Plan Your First Ski Vacation", R.drawable.ic_avt3, "NBC Nightly", "#e1dfdf"));
-        data.add(new Data("How Social Isolation Is Killing Us", R.drawable.ic_avt4, "RBC News", "#e1dfdf"));
-        data.add(new Data("The Labels to Sort Messages in Facebook", R.drawable.ic_avt5, "BBC World", "#f9f9f9"));
-        recyclerView.setAdapter(new NewsAdapter(data));
+    private void initView() {
+        Button btnViewGroup = findViewById(R.id.btnViewGroup);
+        Button btnEventAndListener = findViewById(R.id.btnEventAndListener);
+        btnViewGroup.setOnClickListener(this);
+        btnEventAndListener.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnViewGroup: {
+                Intent intent = new Intent(HomeActivity.this, ViewActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.btnEventAndListener: {
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            }
+            default:
+                break;
+        }
     }
 }
