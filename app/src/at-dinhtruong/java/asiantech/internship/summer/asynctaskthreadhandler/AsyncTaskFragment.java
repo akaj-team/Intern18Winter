0327cd23055297b21347be3dc92d9e9e99fd1dc8
at-dinhtruong <dinh.truong@asiantech.vn>
2018-12-20 17:34:11 +0700

    add thread and fragment

---
+++ app/src/at-dinhtruong/java/asiantech/internship/summer/asynctaskthreadhandler/AsyncTaskFragment.java
@@ -0,0 +1,118 @@
+package asiantech.internship.summer.asynctaskthreadhandler;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import asiantech.internship.summer.R;
+
+
+public class AsyncTaskFragment extends Fragment implements View.OnClickListener {
+    public static final String URL_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Pizigani_1367_Chart_10MB.jpg/1280px-Pizigani_1367_Chart_10MB.jpg";
+    private ImageView mImgImageDownload;
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_asynctask_handler_thread, container, false);
+        initView(view);
+        DownloadTask();
+        return view;
+    }
+
+    private void initView(View view) {
+        mImgImageDownload = view.findViewById(R.id.imgImageDownload);
+        Button btnDownloadAsync = view.findViewById(R.id.btnDownLoadAsync);
+        btnDownloadAsync.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        new DownloadImageTask().execute(URL_IMAGE);
+    }
+
+    public void DownloadTask() {
+        mProgressDialog = new ProgressDialog(getContext());
+        mProgressDialog.setTitle(getString(R.string.asyncTask));
+        mProgressDialog.setMessage(getString(R.string.downloadingFile));
+        mProgressDialog.setIndeterminate(false);
+        mProgressDialog.setMax(100);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        mProgressDialog.setCanceledOnTouchOutside(false);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class DownloadImageTask extends AsyncTask<String, Integer, Bitmap> {
+        long total = 0;
+        int fileLength = 0;
+
+        protected Bitmap doInBackground(String... urls) {
+            Bitmap bitmapImage = null;
+            InputStream inputStream;
+            HttpURLConnection connection;
+            try {
+                URL url = new URL(urls[0]);
+                inputStream = url.openStream();
+                bitmapImage = BitmapFactory.decodeStream(inputStream);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                fileLength = connection.getContentLength();
+                inputStream = connection.getInputStream();
+                byte data[] = new byte[1024];
+                int count;
+                while ((count = inputStream.read(data)) != -1) {
+                    if (isCancelled()) {
+                        inputStream.close();
+                        return null;
+                    }
+                    total += count;
+                    if (fileLength > 0) {
+                        publishProgress((int) (total * 100 / fileLength));
+                    }
+                }
+            } catch (IOException e) {
+                Log.e(getString(R.string.inputOoutputException), getString(R.string.getBitmapFromURL), e);
+            }
+            return bitmapImage;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            mProgressDialog.setMax(100);
+            mProgressDialog.setProgress(values[0]);
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            mProgressDialog.dismiss();
+            mImgImageDownload.setImageBitmap(result);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mProgressDialog.show();
+        }
+    }
+}
--- app/src/at-dinhtruong/java/asiantech/internship/summer/asynctaskthreadhandler/AsyncTaskThreadHandlerActivity.java
+++ app/src/at-dinhtruong/java/asiantech/internship/summer/asynctaskthreadhandler/AsyncTaskThreadHandlerActivity.java
@@ -1,107 +1,29 @@
 package asiantech.internship.summer.asynctaskthreadhandler;
 
-import android.app.ProgressDialog;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import asiantech.internship.summer.R;
 
-public class AsyncTaskThreadHandlerActivity extends AppCompatActivity implements View.OnClickListener {
-    public static final String URL_IMAGE = "https://9mobi.vn/cf/images/2015/03/nkk/anh-dep-1.jpg";
-    private ImageView mImgImageDownload;
-    private ProgressDialog mProgressDialog;
-
+public class AsyncTaskThreadHandlerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async_task_thread_handler);
-        mImgImageDownload = findViewById(R.id.imgImageDownload);
-        Button btnDownloadAsync = findViewById(R.id.btnDownLoadAsync);
-        btnDownloadAsync.setOnClickListener(this);
-        DownloadTask();
-
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setOffscreenPageLimit(2);
+        setupViewPager(viewPager);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    @Override
-    public void onClick(View view) {
-        new DownloadImageTask().execute(URL_IMAGE);
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new AsyncTaskFragment(), getString(R.string.asyncTask));
+        viewPagerAdapter.addFragment(new HandlerFragment(), getString(R.string.handler));
+        viewPagerAdapter.addFragment(new ThreadFragment(), getString(R.string.thread));
+        viewPager.setAdapter(viewPagerAdapter);
     }
-
-    public void DownloadTask() {
-        mProgressDialog = new ProgressDialog(this);
-        mProgressDialog.setMessage("Downloading file..");
-        mProgressDialog.setIndeterminate(false);
-        mProgressDialog.setMax(100);
-        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        mProgressDialog.setCanceledOnTouchOutside(false);
-    }
-
-    private class DownloadImageTask extends AsyncTask<String, Integer, Bitmap> {
-        long total = 0;
-        int fileLength = 0;
-
-        protected Bitmap doInBackground(String... urls) {
-            Bitmap bmp = null;
-            InputStream input = null;
-            HttpURLConnection connection = null;
-            try {
-                URL url = new URL(urls[0]);
-                InputStream inputStream = url.openStream();
-                bmp = BitmapFactory.decodeStream(inputStream);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-                fileLength = connection.getContentLength();
-                input = connection.getInputStream();
-                byte data[] = new byte[1024 / 2];
-
-                int count;
-                while ((count = input.read(data)) != -1) {
-                    if (isCancelled()) {
-                        input.close();
-                        return null;
-                    }
-                    total += count;
-                    if (fileLength > 0) {
-                        try {
-                            Thread.sleep(50);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        publishProgress((int) (total * 100 / fileLength));
-                    }
-                }
-            } catch (Exception e) {
-            }
-            return bmp;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            mProgressDialog.setMax(100);
-            mProgressDialog.setProgress(values[0]);
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            mProgressDialog.dismiss();
-            if (total == fileLength) {
-                mImgImageDownload.setImageBitmap(result);
-            }
-        }
-
-        @Override
-        protected void onPreExecute() {
-            mProgressDialog.show();
-        }
-    }
-
 }
