e9a4f37b6ee4ef9038ed19d667ce4e7e1ed910e2
Bao Le Q <baole@asiantech.vn>
2018-12-26 17:28:04 +0700

    Add 'toViewPagerButton' to List Issues

--- app/src/at-baole/java/asiantech/internship/summer/mainactivity/MainActivity.java
+++ app/src/at-baole/java/asiantech/internship/summer/mainactivity/MainActivity.java
@@ -3,14 +3,18 @@ package asiantech.internship.summer.mainactivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 import asiantech.internship.summer.R;
 import asiantech.internship.summer.activity_fragment.LoginActivity;
 import asiantech.internship.summer.event_listener.SignUpActivity;
 import asiantech.internship.summer.filestorage.FileStorageActivity;
+import asiantech.internship.summer.filestorage.InternalExternalFragment;
 import asiantech.internship.summer.recyclerview.RecyclerViewActivity;
 import asiantech.internship.summer.view_and_groupview.ViewActivity;
+import asiantech.internship.summer.viewpager.PagerActivity;
+import asiantech.internship.summer.viewpager.TimelineActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         Button mBtnToEventListener = findViewById(R.id.btnToEventListener);
         Button mBtnToActivityFragment = findViewById(R.id.btnToActivityFragment);
         Button mBtnToRecyclerView = findViewById(R.id.btnToRecyclerView);
+        Button mBtnToViewPager = findViewById(R.id.btnToViewPager);
         Button mBtnToFileStorage = findViewById(R.id.btnToFileStorage);
 
         mBtnToGroupView.setOnClickListener(toGroupView -> {
@@ -43,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
         mBtnToRecyclerView.setOnClickListener(toRecyclerView -> {
             Intent intentRecyclerView = new Intent(getApplication(), RecyclerViewActivity.class);
             startActivity(intentRecyclerView);
+        });
+
+        mBtnToViewPager.setOnClickListener(toViewPager -> {
+            Intent intentViewPager = new Intent(getApplication(), PagerActivity.class);
+            startActivity(intentViewPager);
         });
 
         mBtnToFileStorage.setOnClickListener(toFileStorage -> {
