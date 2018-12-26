27f584a0645e85ff0fe21abed967986fc5205ece
Bao Le Q <baole@asiantech.vn>
2018-12-26 17:31:46 +0700

    Delete unused imports

--- app/src/at-baole/java/asiantech/internship/summer/mainactivity/MainActivity.java
+++ app/src/at-baole/java/asiantech/internship/summer/mainactivity/MainActivity.java
@@ -3,18 +3,15 @@ package asiantech.internship.summer.mainactivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 
 import asiantech.internship.summer.R;
 import asiantech.internship.summer.activity_fragment.LoginActivity;
 import asiantech.internship.summer.event_listener.SignUpActivity;
 import asiantech.internship.summer.filestorage.FileStorageActivity;
-import asiantech.internship.summer.filestorage.InternalExternalFragment;
 import asiantech.internship.summer.recyclerview.RecyclerViewActivity;
 import asiantech.internship.summer.view_and_groupview.ViewActivity;
 import asiantech.internship.summer.viewpager.PagerActivity;
-import asiantech.internship.summer.viewpager.TimelineActivity;
 
 public class MainActivity extends AppCompatActivity {
 
