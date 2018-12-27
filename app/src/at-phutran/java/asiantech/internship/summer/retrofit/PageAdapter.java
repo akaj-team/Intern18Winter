566a25940e394a931086a52f62585d1768b8a556
Phu Tran D.[4] <phu.tran4@asiantech.vn>
2018-12-27 17:27:41 +0700

    improve tab

--- app/src/at-phutran/java/asiantech/internship/summer/retrofit/PageAdapter.java
+++ app/src/at-phutran/java/asiantech/internship/summer/retrofit/PageAdapter.java
@@ -1,12 +1,15 @@
 package asiantech.internship.summer.retrofit;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import asiantech.internship.summer.viewpager.FavouriteFragment;
 
 public class PageAdapter extends FragmentPagerAdapter {
-    public PageAdapter(FragmentManager fm) {
+    private static final String TAB_LOAD_IMAGE = "Load images";
+    private static final String TAB_INSERT_IMAGE = "Insert images";
+    PageAdapter(FragmentManager fm) {
         super(fm);
     }
 
@@ -19,6 +22,17 @@ public class PageAdapter extends FragmentPagerAdapter {
                 return new FavouriteFragment();
             default:
                 return null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return TAB_LOAD_IMAGE;
+            default:
+                return TAB_INSERT_IMAGE;
         }
     }
 
--- app/src/at-phutran/java/asiantech/internship/summer/retrofit/RetrofitActivity.java
+++ app/src/at-phutran/java/asiantech/internship/summer/retrofit/RetrofitActivity.java
@@ -1,12 +1,10 @@
 package asiantech.internship.summer.retrofit;
 
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import asiantech.internship.summer.R;
-import asiantech.internship.summer.viewpager.FavouriteFragment;
 
 public class RetrofitActivity extends AppCompatActivity {
 
@@ -17,33 +15,9 @@ public class RetrofitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_retofit_gson);
         TabLayout mTabLayoutHeader = findViewById(R.id.tabLayoutHeader);
         mViewPagerContent = findViewById(R.id.viewPagerContent);
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.commit();
         PageAdapter mPageAdapter = new PageAdapter(getSupportFragmentManager());
         mViewPagerContent.setAdapter(mPageAdapter);
-        mTabLayoutHeader.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                mViewPagerContent.setCurrentItem(tab.getPosition());
-                if (tab.getPosition() == 0) {
-                    fragmentTransaction.replace(R.id.viewPagerContent, new FragmentLoadImage());
-                } else {
-                    FavouriteFragment favouriteFragment = new FavouriteFragment();
-                    fragmentTransaction.replace(R.id.viewPagerContent, favouriteFragment);
-                }
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-        mViewPagerContent.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayoutHeader));
+        mTabLayoutHeader.setupWithViewPager(mViewPagerContent);
     }
 
 }
