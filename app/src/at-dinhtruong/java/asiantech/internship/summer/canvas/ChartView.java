000fb80a739b112c4294081f0f64a68dd0c61b72
at-dinhtruong <dinh.truong@asiantech.vn>
2019-01-01 22:46:18 +0700

    display chart

--- app/src/at-dinhtruong/java/asiantech/internship/summer/canvas/ChartView.java
+++ app/src/at-dinhtruong/java/asiantech/internship/summer/canvas/ChartView.java
@@ -1,13 +1,12 @@
 package asiantech.internship.summer.canvas;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.util.AttributeSet;
-import android.view.Display;
+import android.util.DisplayMetrics;
 import android.view.View;
-import android.view.WindowManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,15 +15,10 @@ import asiantech.internship.summer.R;
 import asiantech.internship.summer.models.Money;
 
 public class ChartView extends View {
-    private Paint mPaintSale;
-    private Paint mPaintExpense;
-    private Paint mPaintText;
-    private Paint mPaintTextMoney;
-    private Paint mPaintTextContent;
+    private Paint mPaintTextItem;
+    private Paint mPaintTextCaption;
     private Paint mPaintLine;
-    private Paint mPaintNoteSale;
-    private Paint mPaintNoteExpense;
-    private Paint mPaint;
+    private Paint mPaintColumn;
     private List<Money> mListMoney;
 
     public ChartView(Context context, AttributeSet attrs) {
@@ -33,45 +27,29 @@ public class ChartView extends View {
 
     public ChartView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         initPaint();
-        initListMoney();
+        createListMoney();
     }
 
-    private void initListMoney() {
-        mListMoney = new ArrayList<>();
-        mListMoney.add(new Money("Jan", 70000, 10000));
-        mListMoney.add(new Money("Feb", 80000, 18000));
-        mListMoney.add(new Money("Mar", 75000, 20000));
-        mListMoney.add(new Money("Apr", 90000, 30000));
-        mListMoney.add(new Money("May", 105000, 42000));
-        mListMoney.add(new Money("Jun", 130000, 80000));
+    private void initPaint() {
+        mPaintTextItem = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintTextItem = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        mPaintColumn = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintColumn.setColor(getResources().getColor(R.color.colorBlue));
+        mPaintColumn.setStrokeWidth(getHeightScreen() / 30);
     }
 
     private int getWidthScreen() {
-        WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
-        assert windowManager != null;
-        Display display = windowManager.getDefaultDisplay();
-        Point size = new Point();
-        try {
-            display.getRealSize(size);
-        } catch (NoSuchMethodError err) {
-            display.getSize(size);
-        }
-        return size.x;
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
     }
 
     private int getHeightScreen() {
-        WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
-        assert windowManager != null;
-        Display display = windowManager.getDefaultDisplay();
-        Point size = new Point();
-        try {
-            display.getRealSize(size);
-        } catch (NoSuchMethodError err) {
-            display.getSize(size);
-        }
-        return size.y;
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
     }
 
     @Override
@@ -79,88 +57,61 @@ public class ChartView extends View {
         super.onDraw(canvas);
         int width = getWidthScreen();
         int height = getHeightScreen();
-        int x = getWidthScreen() / 13;
-        int y = 4 * getHeightScreen() / 5;
-        drawColumnChart(width, height, x, y, canvas);
-        drawLineMoney(width, height, x, y, canvas);
-        canvas.drawText(getContext().getString(R.string.salesAndExpenses), getWidthScreen() / 2 - mPaintTextContent.measureText(getContext().getString(R.string.salesAndExpenses)) / 2, getHeightScreen() / 20, mPaintTextContent);
-        canvas.drawLine(getWidthScreen() / 2 - mPaintTextContent.measureText(getContext().getString(R.string.expenses)) - getHeightScreen() / 40, 9 * getHeightScreen() / 10, getWidthScreen() / 2 - mPaintTextContent.measureText(getContext().getString(R.string.expenses)) - getHeightScreen() / 40, 9 * getHeightScreen() / 10 - 30, mPaintNoteSale);
-        canvas.drawText(getContext().getString(R.string.sales), getWidthScreen() / 2 - mPaintTextContent.measureText(getContext().getString(R.string.expenses)), 9 * getHeightScreen() / 10, mPaintText);
-        canvas.drawLine(getWidthScreen() / 2, 9 * getHeightScreen() / 10, getWidthScreen() / 2, 9 * getHeightScreen() / 10 - 30, mPaintNoteExpense);
-        canvas.drawText(getContext().getString(R.string.expenses), getWidthScreen() / 2 + getHeightScreen() / 40, 9 * getHeightScreen() / 10, mPaintText);
+        int xDistance = width / 12;
+        int yDistance = 4 * getHeightScreen() / 5;
+        drawColumnChart(width, height, xDistance, yDistance, canvas);
+        drawLineMoney(width, height, xDistance, yDistance, canvas);
+        mPaint.setTextSize(50);
+        canvas.drawText(getContext().getString(R.string.salesAndExpenses), (float) ((width) / (2.5)), height / 20, mPaint);
+        //draw note
+        mPaint.reset();
+        mPaint.setColor(getResources().getColor(R.color.colorBlue));
+        canvas.drawRect((float) (width / 2.5), height - 130, (float) (width / 2.5 + 30), height - 100, mPaint);
+        mPaint.setColor(getResources().getColor(R.color.colorBlue));
+        canvas.drawRect((float) (width / 2.5 + 180), height - 130, (float) (width / 2.5 + 210), height - 100, mPaint);
+        mPaint.setColor(getResources().getColor(R.color.colorBlack));
+        mPaint.setTextSize(35);
+        canvas.drawText(getContext().getString(R.string.sales), (float) (width / 2.5 + 60), height - 100, mPaint);
+        canvas.drawText(getContext().getString(R.string.expenses), (float) (width / 2.5 + 240), height - 100, mPaint);
+
     }
 
-    private void drawLineMoney(int width, int height, int x, int y, Canvas canvas) {
-        for (int i = 0; i < 8; i++) {
-            if (i == 0) {
-                //test
-                mPaint.setColor(getResources().getColor(R.color.colorBlack));
-                mPaint.setTextAlign(Paint.Align.RIGHT);
-                mPaint.setTextSize(30);
-                canvas.drawText("0", 2 * width / 30, y - height / 10 * i, mPaint);
-                mPaint.reset();
-                mPaint.setColor(getResources().getColor(R.color.colorBlack));
-                canvas.drawLine(x, y, width, y, mPaint);
-            } else {
-                //test
-                mPaint.setColor(getResources().getColor(R.color.colorBlack));
-                mPaint.setTextAlign(Paint.Align.RIGHT);
-                mPaint.setTextSize(30);
-                canvas.drawText("$" + 20 * i + ",000", 2 * width / 30, y - height / 10 * i, mPaint);
-                mPaint.reset();
-                mPaint.setColor(getResources().getColor(R.color.colorBlack));
-                canvas.drawLine(x, y - height / 10 * i, width, y - height / 10 * i, mPaint);
-            }
+    private void drawLineMoney(int width, int height, int xDistance, int yDistance, Canvas canvas) {
+        mPaint.setColor(getResources().getColor(R.color.colorBlack));
+        mPaint.setTextAlign(Paint.Align.RIGHT);
+        mPaint.setTextSize(35);
+        canvas.drawText("0", xDistance, yDistance, mPaint);
+        canvas.drawLine(xDistance + xDistance / 3, yDistance, width, yDistance, mPaint);
+        for (int i = 1; i < 8; i++) {
+            canvas.drawText("$ " + 20000 * i, xDistance, yDistance - yDistance / 8 * i, mPaint);
+            canvas.drawLine(xDistance + xDistance / 3, yDistance - height / 10 * i, width, yDistance - height / 10 * i, mPaint);
         }
+        mPaint.reset();
     }
 
-    private void drawColumnChart(int width, int height, int x, int y, Canvas canvas) {
+    private void drawColumnChart(int width, int height, int xDistance, int yDistance, Canvas canvas) {
         for (int i = 0; i < mListMoney.size(); i++) {
             mPaint.setColor(getResources().getColor(R.color.colorBlue));
-            mPaint.setStrokeWidth(getHeightScreen() / 30);
-            canvas.drawLine(x + 250 * i + width / 20, y, x + 250 * i + width / 20, y - (mListMoney.get(i).getSale() * height) / 200000, mPaint);
+            mPaint.setStrokeWidth(height / 30);
+            canvas.drawLine((float) (1.8 * xDistance + 250 * i), yDistance, (float) (1.8 * xDistance + 250 * i), yDistance - (mListMoney.get(i).getSale() * height) / 200000, mPaint);
             mPaint.setColor(getResources().getColor(R.color.colorGray));
-            mPaint.setStrokeWidth(getHeightScreen() / 30);
-            canvas.drawLine(x + 250 * i + width / 20 + width / 30, y, x + 250 * i + width / 30 + width / 20, (y - mListMoney.get(i).getExpense() * height / 200000), mPaint);
+            mPaint.setStrokeWidth(height / 30);
+            canvas.drawLine((float) (2.1 * xDistance + 250 * i), yDistance, (float) (2.1 * xDistance + 250 * i), (yDistance - mListMoney.get(i).getExpense() * height / 200000), mPaint);
             mPaint.reset();
             mPaint.setColor(getResources().getColor(R.color.colorBlack));
             mPaint.setTextSize(30);
-            canvas.drawText(mListMoney.get(i).getMonth(), x + 250 * i + width / 20 + width / 190, y + height / 30, mPaint);
+            canvas.drawText(mListMoney.get(i).getMonth(), (float) (1.8 * xDistance + 250 * i), yDistance + yDistance / 20, mPaint);
         }
     }
 
-    private void initPaint() {
-        mPaintSale = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintSale.setColor(getResources().getColor(R.color.colorBlue));
-        mPaintSale.setStrokeWidth(getHeightScreen() / 30);
-
-        mPaintExpense = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintExpense.setColor(getResources().getColor(R.color.colorGray));
-        mPaintExpense.setStrokeWidth(getHeightScreen() / 30);
-
-        mPaintText = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintText.setColor(getResources().getColor(R.color.colorBlack));
-        mPaintText.setTextSize(30);
-
-        mPaintTextMoney = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintTextMoney.setColor(getResources().getColor(R.color.colorBlack));
-        mPaintTextMoney.setTextAlign(Paint.Align.RIGHT);
-        mPaintTextMoney.setTextSize(30);
-
-        mPaintLine = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintLine.setColor(getResources().getColor(R.color.colorBlack));
-
-        mPaintTextContent = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintTextContent.setColor(getResources().getColor(R.color.colorBlack));
-        mPaintTextContent.setTextSize(50);
-
-        mPaintNoteSale = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintNoteSale.setColor(getResources().getColor(R.color.colorBlue));
-        mPaintNoteSale.setStrokeWidth(getHeightScreen() / 40);
-
-        mPaintNoteExpense = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintNoteExpense.setColor(getResources().getColor(R.color.colorGray));
-        mPaintNoteExpense.setStrokeWidth(getHeightScreen() / 40);
+    private void createListMoney() {
+        mListMoney = new ArrayList<>();
+        mListMoney.add(new Money("Jan", 70000, 10000));
+        mListMoney.add(new Money("Feb", 80000, 18000));
+        mListMoney.add(new Money("Mar", 75000, 20000));
+        mListMoney.add(new Money("Apr", 90000, 30000));
+        mListMoney.add(new Money("May", 105000, 42000));
+        mListMoney.add(new Money("Jun", 130000, 80000));
     }
 }
 
