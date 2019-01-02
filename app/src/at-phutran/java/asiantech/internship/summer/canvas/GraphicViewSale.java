d6d5cb3f81b226d5ee05fdcb3a8d1c213d348d6e
Phu Tran D.[4] <phu.tran4@asiantech.vn>
2019-01-02 17:15:26 +0700

    zoom and scroll

--- app/src/at-phutran/java/asiantech/internship/summer/canvas/GraphicViewSale.java
+++ app/src/at-phutran/java/asiantech/internship/summer/canvas/GraphicViewSale.java
@@ -1,5 +1,6 @@
 package asiantech.internship.summer.canvas;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -10,10 +11,8 @@ import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.WindowManager;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import asiantech.internship.summer.R;
 import asiantech.internship.summer.model.Money;
 
@@ -21,9 +20,12 @@ public class GraphicViewSale extends View {
     private static final String CONTENT = "Sales and Expenses";
     private static final String SALE = "Sales";
     private static final String EXPENSE = "Expenses";
+    private static final String DEFAULT_STRING = "$140,000";
     private static final int DRAG = 1;
     private static final int NONE = 0;
     private static final int ZOOM = 2;
+    private int mBeginWidth;
+    private int mBeginHeight;
     private boolean mDragged = false;
     private Paint mPaintSale;
     private Paint mPaintExpense;
@@ -33,6 +35,7 @@ public class GraphicViewSale extends View {
     private Paint mPaintLine;
     private Paint mPaintNoteSale;
     private Paint mPaintNoteExpense;
+    private Paint mPaintBackground;
     private int mMode;
     private float mStartX = 0f;
     private float mStartY = 0f;
@@ -69,21 +72,21 @@ public class GraphicViewSale extends View {
     private void initListMoney() {
         mListMoney = new ArrayList<>();
         //10 000$ = 1/20 height
-        mListMoney.add(new Money("Jan", 70, 10));
-        mListMoney.add(new Money("Feb", 80, 15));
-        mListMoney.add(new Money("Mar", 75, 20));
-        mListMoney.add(new Money("Apr", 90, 30));
-        mListMoney.add(new Money("May", 105, 45));
-        mListMoney.add(new Money("June", 130, 80));
-        mListMoney.add(new Money("Jul", 115, 50));
-        mListMoney.add(new Money("Aug", 100, 40));
-        mListMoney.add(new Money("Sep", 90, 20));
-        mListMoney.add(new Money("Oct", 80, 20));
-        mListMoney.add(new Money("Nov", 120, 40));
-        mListMoney.add(new Money("Dec", 140, 80));
+        mListMoney.add(new Money(getContext().getString(R.string.Jan), 70, 10));
+        mListMoney.add(new Money(getContext().getString(R.string.Feb), 80, 15));
+        mListMoney.add(new Money(getContext().getString(R.string.Mar), 75, 20));
+        mListMoney.add(new Money(getContext().getString(R.string.Apr), 90, 30));
+        mListMoney.add(new Money(getContext().getString(R.string.May), 105, 45));
+        mListMoney.add(new Money(getContext().getString(R.string.June), 130, 80));
+        mListMoney.add(new Money(getContext().getString(R.string.Jul), 115, 50));
+        mListMoney.add(new Money(getContext().getString(R.string.Aug), 100, 40));
+        mListMoney.add(new Money(getContext().getString(R.string.Sep), 90, 20));
+        mListMoney.add(new Money(getContext().getString(R.string.Oct), 80, 20));
+        mListMoney.add(new Money(getContext().getString(R.string.Nov), 120, 40));
+        mListMoney.add(new Money(getContext().getString(R.string.Dec), 140, 80));
     }
 
-    private int getWithScreen() {
+    private int getWidthScreen() {
         WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
         assert windowManager != null;
         Display display = windowManager.getDefaultDisplay();
@@ -109,9 +112,9 @@ public class GraphicViewSale extends View {
         return size.y;
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        // Let the ScaleGestureDetector inspect all events.
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
                 mMode = DRAG;
@@ -158,35 +161,46 @@ public class GraphicViewSale extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        mBeginWidth = getWidthScreen() / 10;
+        mBeginHeight = 4 * getHeightScreen() / 5;
         canvas.save();
         canvas.scale(mScaleFactor, mScaleFactor, mScaleDetector.getFocusX(), mScaleDetector.getFocusY());
+        drawNoteChart(canvas);
+
         canvas.save();
         canvas.translate(mTranslateX / mScaleFactor, 0);
-        //Draw component in here
-        int x = getWithScreen() / 10;
-        int y = 4 * getHeightScreen() / 5;
-        for (int i = 0; i < mListMoney.size(); i++) {
-            canvas.drawLine(x + 250 * i + getWithScreen() / 50, y, x + 250 * i + getWithScreen() / 50, (y - mListMoney.get(i).getSale() * getHeightScreen() / 200), mPaintSale);
-            canvas.drawLine(x + 250 * i + getWithScreen() / 50 + getWithScreen() / 30, y, x + 250 * i + getWithScreen() / 30 + getWithScreen() / 50, (y - mListMoney.get(i).getExpense() * getHeightScreen() / 200), mPaintExpense);
-            canvas.drawText(mListMoney.get(i).getMonth(), x + 250 * i + getWithScreen() / 50 + getHeightScreen() / 70, y + getHeightScreen() / 40, mPaintText);
-        }
-        for (int i = 0; i < 8; i++) {
-            if (i == 0) {
-                canvas.drawText(getContext().getString(R.string.zero), 2 * getWithScreen() / 30, y - getHeightScreen() / 10 * i, mPaintTextMoney);
-                canvas.drawLine(x - getWithScreen() / 50, y, 2 * getWithScreen(), y, mPaintLine);
-            } else {
-                canvas.drawText(getContext().getString(R.string.dola) + 20 * i + getContext().getString(R.string.thousan), 2 * getWithScreen() / 30, y - getHeightScreen() / 10 * i, mPaintTextMoney);
-                canvas.drawLine(x - getWithScreen() / 50, y - getHeightScreen() / 10 * i, 2 * getWithScreen(), y - getHeightScreen() / 10 * i, mPaintLine);
-            }
-        }
-        canvas.drawText(CONTENT, getWithScreen() / 2 - mPaintTextContent.measureText(CONTENT) / 2, getHeightScreen() / 20, mPaintTextContent);
-        canvas.drawLine(getWithScreen() / 2 - mPaintTextContent.measureText(EXPENSE) - getHeightScreen() / 40, 9 * getHeightScreen() / 10, getWithScreen() / 2 - mPaintTextContent.measureText(EXPENSE) - getHeightScreen() / 40, 9 * getHeightScreen() / 10 - 30, mPaintNoteSale);
-        canvas.drawText(SALE, getWithScreen() / 2 - mPaintTextContent.measureText(EXPENSE), 9 * getHeightScreen() / 10, mPaintText);
-        canvas.drawLine(getWithScreen() / 2, 9 * getHeightScreen() / 10, getWithScreen() / 2, 9 * getHeightScreen() / 10 - 30, mPaintNoteExpense);
-        canvas.drawText(EXPENSE, getWithScreen() / 2 + getHeightScreen() / 40, 9 * getHeightScreen() / 10, mPaintText);
+        drawChart(canvas);
         canvas.restore();
+        drawLineAndSale(canvas);
     }
 
+    private void drawNoteChart(Canvas canvas) {
+        canvas.drawText(CONTENT, getWidthScreen() / 2 - mPaintTextContent.measureText(CONTENT) / 2, getHeightScreen() / 20, mPaintTextContent);
+        canvas.drawLine(getWidthScreen() / 2 - mPaintTextContent.measureText(EXPENSE) - getHeightScreen() / 40, 9 * getHeightScreen() / 10, getWidthScreen() / 2 - mPaintTextContent.measureText(EXPENSE) - getHeightScreen() / 40, 9 * getHeightScreen() / 10 - 30, mPaintNoteSale);
+        canvas.drawText(SALE, getWidthScreen() / 2 - mPaintTextContent.measureText(EXPENSE), 9 * getHeightScreen() / 10, mPaintText);
+        canvas.drawLine(getWidthScreen() / 2, 9 * getHeightScreen() / 10, getWidthScreen() / 2, 9 * getHeightScreen() / 10 - 30, mPaintNoteExpense);
+        canvas.drawText(EXPENSE, getWidthScreen() / 2 + getHeightScreen() / 40, 9 * getHeightScreen() / 10, mPaintText);
+    }
+
+    private void drawLineAndSale(Canvas canvas) {
+        canvas.drawRect(0, 0, mBeginWidth - getWidthScreen() / 50, getHeightScreen(), mPaintBackground);
+        for (int i = 0; i < 8; i++) {
+            if (i == 0) {
+                canvas.drawText(getContext().getString(R.string.zero), mPaintTextMoney.measureText(DEFAULT_STRING), mBeginHeight - getHeightScreen() / 10 * i, mPaintTextMoney);
+                canvas.drawLine(mBeginWidth - getWidthScreen() / 50, mBeginHeight, getWidthScreen(), mBeginHeight, mPaintLine);
+            } else {
+                canvas.drawText(getContext().getString(R.string.dola) + 20 * i + getContext().getString(R.string.thousan), mPaintTextMoney.measureText(DEFAULT_STRING), mBeginHeight - getHeightScreen() / 10 * i, mPaintTextMoney);
+                canvas.drawLine(mBeginWidth - getWidthScreen() / 50, mBeginHeight - getHeightScreen() / 10 * i, getWidthScreen(), mBeginHeight - getHeightScreen() / 10 * i, mPaintLine);
+            }
+        }
+    }
+    private void drawChart(Canvas canvas) {
+        for (int i = 0; i < mListMoney.size(); i++) {
+            canvas.drawLine(mBeginWidth + 250 * i + getWidthScreen() / 50, mBeginHeight, mBeginWidth + 250 * i + getWidthScreen() / 50, (mBeginHeight - mListMoney.get(i).getSale() * getHeightScreen() / 200), mPaintSale);
+            canvas.drawLine(mBeginWidth + 250 * i + getWidthScreen() / 50 + getWidthScreen() / 30, mBeginHeight, mBeginWidth + 250 * i + getWidthScreen() / 30 + getWidthScreen() / 50, (mBeginHeight - mListMoney.get(i).getExpense() * getHeightScreen() / 200), mPaintExpense);
+            canvas.drawText(mListMoney.get(i).getMonth(), mBeginWidth + 250 * i + getWidthScreen() / 50 + getHeightScreen() / 70, mBeginHeight + getHeightScreen() / 40, mPaintText);
+        }
+    }
     private void setChartSize(int width, int height) {
         if ((mTranslateX * -1) < 0) {
             mTranslateX = 0;
@@ -202,20 +216,19 @@ public class GraphicViewSale extends View {
     }
 
     private int getMaxChartWidth() {
-        int count = mListMoney.size();
-        return (count * 60 + (count - 1) * 180);
+        return mBeginWidth + 250 * 11 + getWidthScreen() / 50;
     }
 
     private void initPaintSale() {
         mPaintSale = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintSale.setColor(getResources().getColor(R.color.colorColumnBlue));
-        mPaintSale.setStrokeWidth(getHeightScreen() / 20);
+        mPaintSale.setStrokeWidth(getWidthScreen() / 40);
     }
 
     private void initPaintExpense() {
         mPaintExpense = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintExpense.setColor(getResources().getColor(R.color.colorColumnOrange));
-        mPaintExpense.setStrokeWidth(getHeightScreen() / 20);
+        mPaintExpense.setStrokeWidth(getWidthScreen() / 40);
     }
 
     private void initPaintText() {
@@ -233,7 +246,9 @@ public class GraphicViewSale extends View {
 
     private void initPaintLine() {
         mPaintLine = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintBackground = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintLine.setColor(getResources().getColor(R.color.colorLineGray));
+        mPaintBackground.setColor(getResources().getColor(R.color.colorWhite));
     }
 
     private void initPaintTextContent() {
@@ -245,20 +260,19 @@ public class GraphicViewSale extends View {
     private void initPaintNoteSale() {
         mPaintNoteSale = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintNoteSale.setColor(getResources().getColor(R.color.colorColumnBlue));
-        mPaintNoteSale.setStrokeWidth(getHeightScreen() / 40);
+        mPaintNoteSale.setStrokeWidth(getWidthScreen() / 80);
     }
 
     private void initPaintNoteExpense() {
         mPaintNoteExpense = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintNoteExpense.setColor(getResources().getColor(R.color.colorColumnOrange));
-        mPaintNoteExpense.setStrokeWidth(getHeightScreen() / 40);
+        mPaintNoteExpense.setStrokeWidth(getWidthScreen() / 80);
     }
 
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
             mScaleFactor *= detector.getScaleFactor();
-            // Don't let the object get too small or too large.
             mScaleFactor = Math.max(1.0f, Math.min(mScaleFactor, 5.0f));
             invalidate();
             return true;
