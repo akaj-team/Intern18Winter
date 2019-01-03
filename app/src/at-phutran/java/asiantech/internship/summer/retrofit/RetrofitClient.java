9d15b5ba57f9245aca65b15ad535c4f621ea73b7
Phu Tran D.[4] <phu.tran4@asiantech.vn>
2019-01-03 11:29:02 +0700

    retrofit

--- app/src/at-phutran/java/asiantech/internship/summer/retrofit/RetrofitClient.java
+++ app/src/at-phutran/java/asiantech/internship/summer/retrofit/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 class RetrofitClient {
-    private static String BASE_URL = "https://api.gyazo.com/api/";
+    private static final String BASE_URL = "https://api.gyazo.com/api/";
     private static Retrofit retrofit = null;
 
     static Retrofit getClient() {
