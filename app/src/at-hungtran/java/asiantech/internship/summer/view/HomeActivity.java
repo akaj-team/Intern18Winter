442e5921c5d63d6205aa142070a6c5f2ff8d0f06
Hung Tran V.[2] <hung.tran2@asiantech.vn>
2019-01-03 13:37:15 +0700

    delete menu

--- app/src/at-hungtran/java/asiantech/internship/summer/view/HomeActivity.java
+++ app/src/at-hungtran/java/asiantech/internship/summer/view/HomeActivity.java
@@ -1,68 +1,37 @@
 package asiantech.internship.summer.view;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import asiantech.internship.summer.R;
-import asiantech.internship.summer.eventlistener.LoginActivity;
-import asiantech.internship.summer.filestorage.FileStorageActivity;
-import asiantech.internship.summer.recyclerview.TimelineActivity;
+import asiantech.internship.summer.model.Data;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeActivity extends AppCompatActivity {
+    private static final int COLUMNS = 2;
+    List<Data> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_layoutfb);
         initView();
     }
 
-    private void initView() {
-        Button btnViewGroup = findViewById(R.id.btnViewGroup);
-        Button btnEventAndListener = findViewById(R.id.btnEventAndListener);
-        Button btnActivityFragment = findViewById(R.id.btnActivityFragment);
-        Button btnRecyclerView = findViewById(R.id.btnRecyclerView);
-        Button btnFileStorage = findViewById(R.id.btnFileStorage);
-        btnViewGroup.setOnClickListener(this);
-        btnEventAndListener.setOnClickListener(this);
-        btnActivityFragment.setOnClickListener(this);
-        btnRecyclerView.setOnClickListener(this);
-        btnFileStorage.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnViewGroup: {
-                Intent intent = new Intent(HomeActivity.this, ViewActivity.class);
-                startActivity(intent);
-                break;
-            }
-            case R.id.btnEventAndListener: {
-                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
-                startActivity(intent);
-                break;
-            }
-            case R.id.btnActivityFragment: {
-                Intent intent = new Intent(HomeActivity.this, asiantech.internship.summer.activityfragment.LoginActivity.class);
-                startActivity(intent);
-                break;
-            }
-            case R.id.btnRecyclerView: {
-                Intent intent = new Intent(HomeActivity.this, TimelineActivity.class);
-                startActivity(intent);
-                break;
-            }
-            case R.id.btnFileStorage: {
-                Intent intent = new Intent(HomeActivity.this, FileStorageActivity.class);
-                startActivity(intent);
-                break;
-            }
-            default:
-                break;
-        }
+    public void initView() {
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, COLUMNS));
+        data.add(new Data("A city Living Under the Shadow", R.drawable.ic_avt, "RBC News", "#e1dfdf"));
+        data.add(new Data("The Problem for Democratic Leaders", R.drawable.ic_avt1, "NY Times", "#f9f9f9"));
+        data.add(new Data("The Golden Secret to Better Beakfast", R.drawable.ic_avt2, "BBC World", "#f9f9f9"));
+        data.add(new Data("How to Plan Your First Ski Vacation", R.drawable.ic_avt3, "NBC Nightly", "#e1dfdf"));
+        data.add(new Data("How Social Isolation Is Killing Us", R.drawable.ic_avt4, "RBC News", "#e1dfdf"));
+        data.add(new Data("The Labels to Sort Messages in Facebook", R.drawable.ic_avt5, "BBC World", "#f9f9f9"));
+        recyclerView.setAdapter(new NewsAdapter(data));
     }
 }
